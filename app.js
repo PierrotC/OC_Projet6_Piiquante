8e63abe8ce7e93da4906f7c014d593135a8d9fd9
PierrotC <pierre-courteille@outlook.fr>
2022-07-09 19:00:14 +0200

    setup the user routes and controllers: login and signup

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const app = express();
+const userRoutes = require('./routes/user');
 
-mongoose.connect('mongodb+srv://PierrotC:KBmQd5j4UT77Pkt@ocpc.nbxrm.mongodb.net/?retryWrites=true&w=majority',
+const app = express();
+mongoose.connect('mongodb+srv://PierrotC:ky8DtCSUG1dZv5LQ@cluster0.n0f7a.mongodb.net/?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -13,5 +14,14 @@ mongoose.connect('mongodb+srv://PierrotC:KBmQd5j4UT77Pkt@ocpc.nbxrm.mongodb.net/
     .catch(() => console.log('Failed to connect to MongoDB...'));
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || '3000');
+const port = process.env.PORT || '3000';
+
+server.listen(port);
+
+console.log("Listening on port : " + port);
