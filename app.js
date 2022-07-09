4a7c7b7436b8a75ddd76cb60ca48e38e450c2d80
PierrotC <pierre-courteille@outlook.fr>
2022-07-09 17:38:25 +0200

    initialized user schema / routes and controllers folders

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -10,5 +11,7 @@ mongoose.connect('mongodb+srv://PierrotC:KBmQd5j4UT77Pkt@ocpc.nbxrm.mongodb.net/
 })
     .then(() => console.log('MongoDB connected successfully!'))
     .catch(() => console.log('Failed to connect to MongoDB...'));
+
+app.use(bodyParser.json());
 
 module.exports = app;
