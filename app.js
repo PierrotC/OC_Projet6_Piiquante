ee25b85967a70dce4d128ee9dce939fbafbb132b
PierrotC <pierre-courteille@outlook.fr>
2022-07-09 17:20:15 +0200

    Initialized mongoose, mongodb

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-modules.exports = app;
+mongoose.connect('mongodb+srv://PierrotC:KBmQd5j4UT77Pkt@ocpc.nbxrm.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+})
+    .then(() => console.log('MongoDB connected successfully!'))
+    .catch(() => console.log('Failed to connect to MongoDB...'));
+
+module.exports = app;
